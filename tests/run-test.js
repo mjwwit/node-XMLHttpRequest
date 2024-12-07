900dc9d1a98b5eb878b43ee5fc0acc81dcad90fa
bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2024-12-07 19:27:26 +0900

    change identation of test runner

--- tests/run-test.js
+++ tests/run-test.js
@@ -1,6 +1,6 @@
 var ignored_files = [
-	"run-test.js", // this file
-	"server.js"
+  "run-test.js", // this file
+  "server.js"
 ];
 
 var spawnSync = require("child_process").spawnSync;
@@ -12,34 +12,34 @@ var fail_path = false;
 
 // function to read and conduct test case
 var run_test = function (file) {
-	if (fail_path) return;
-	// logging
-	console.log("Running:", file);
+  if (fail_path) return;
+  // logging
+  console.log("Running:", file);
 
-	// spawn a nodejs process
-	var proc = spawnSync("node", [file]);
+  // spawn a nodejs process
+  var proc = spawnSync("node", [file]);
 
-	if (proc.status === 0) {
-		console.log(proc.stdout.toString());
-		console.log("--> PASSED");
-	}
-	else {
-		fail_path = file;
-		console.log("--> TEST FAILED - CAUGHT ERROR:", proc.stderr.toString());
-	}
+  if (proc.status === 0) {
+    console.log(proc.stdout.toString());
+    console.log("--> PASSED");
+  }
+  else {
+    fail_path = file;
+    console.log("--> TEST FAILED - CAUGHT ERROR:", proc.stderr.toString());
+  }
 }
 
 var check_dir = function (dirPath) {
-	if (fail_path) return;
-	var files = fs.readdirSync(dirPath);
+  if (fail_path) return;
+  var files = fs.readdirSync(dirPath);
 
-	for (var file of files) {
-		// return early in case something fails
-		if (fail_path) return;
-		var full_path = path.join(dirPath, file);
-		if (fs.statSync(full_path).isDirectory()) check_dir(full_path);
-		else if (path.extname(file) === ".js" && !ignored_files.includes(full_path)) run_test(full_path);
-	}
+  for (var file of files) {
+    // return early in case something fails
+    if (fail_path) return;
+    var full_path = path.join(dirPath, file);
+    if (fs.statSync(full_path).isDirectory()) check_dir(full_path);
+    else if (path.extname(file) === ".js" && !ignored_files.includes(full_path)) run_test(full_path);
+  }
 }
 
 // start test
